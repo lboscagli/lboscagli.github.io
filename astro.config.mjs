c0e4c869356cae866ccdc7fe0f656fdaa856a87a
Luca <l.boscagli@imperial.ac.uk>
2026-06-04 11:38:15 +0100

    Configure GitHub Pages Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lboscagli.github.io/",
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
